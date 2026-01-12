f4ebbbea020649fe287029b7b7ee45a7ad754fa6
labcabrera <lab.cabrera@gmail.com>
2026-01-12 11:16:42 +0100

    Minor.

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,8 @@ docker stop azure-speech-api
 
 docker build -t azure-speech-api .
 
-docker run --rm -p 8000:8000 -e SPEECH_KEY="$SPEECH_KEY" -e SPEECH_REGION="$SPEECH_REGION" azure-speech-api
+docker run --rm \
+    -p 8000:8000 \
+    -e SPEECH_KEY="$SPEECH_KEY" \
+    -e SPEECH_REGION="$SPEECH_REGION" \
+    azure-speech-api
